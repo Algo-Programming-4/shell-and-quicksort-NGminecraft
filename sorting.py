03b4fdcdd4ed5307a47e6919df9a3f1530f5f9a5
NGminecraft <grahanic001@alpinesd.org>
2024-10-07 14:06:10 -0600

    Starting to get shell

--- sorting.py
+++ sorting.py
@@ -1,9 +1,51 @@
-#bubble(list) -> sorted ist
+import math
+
+#bubble(list) - > sorted list
+def bubble(lst):
+    for i in range(len(lst)-1):
+        for i, v in enumerate(lst[0:-1]):
+            if v > lst[i+1]:
+                lst[i], lst[i+1]= lst[i+1], lst[i]
+    return lst
+
 
 #selection(list) -> sorted list
+def selection(lst):
+    for full_index in range(len(lst)-1):
+        index_smallest = full_index+1
+        for index, value in enumerate(lst[full_index:]):
+            if value < lst[index_smallest]:
+                index_smallest = index+full_index
+        lst[index_smallest], lst[full_index] = lst[full_index], lst[index_smallest]
+    return lst
 
-#insertion(list) -> sorted list
 
-#shell(list) -> sorted list
+#insertion(list) > sorted list
+def insertion(lst:list):   
+    for main_index, main_value in enumerate(lst[1:]):
+        index = 0
+        while main_value > lst[index]:
+            index+=1
+        popped = lst.pop(main_index+1)
+        lst.insert(index, popped)
+    return lst
+
+#shell(List) -> sorted list
+def shell(lst):
+    gaps = []
+    length = len(lst)
+    while length > 1:
+        length = length//2
+        gaps.append(length)
+    print(gaps)
+    for gap in gaps:
+        lst_starting_indices = list(range(len(lst)-1-gap))
+        for i in lst_starting_indices:
+            
 
 #quicksort(list) -> sorted list
+def quicksort(lst):
+    pass
+
+
+print(shell([5,4,3,2,1]))
---
+++ test_sorting.py
@@ -0,0 +1,42 @@
+import sorting
+import random
+import unittest
+
+
+nums = [random.randint(1, 100) for _ in range(random.randint(1, 10))]
+sorted_nums = sorted(nums)
+
+class test_sorting(unittest.TestCase):
+    def test_bubble(self):
+        print(nums)
+        result = sorting.bubble(nums)
+        self.assertIsNotNone(result)
+        self.assertEqual(result, sorted_nums)
+            
+    def test_selection(self):
+        result = sorting.selection(nums)
+        self.assertIsNotNone(result)
+        self.assertEqual(result, sorted_nums)
+            
+    def test_insertion(self):
+        result = sorting.insertion(nums)
+        self.assertIsNotNone(result)
+        self.assertEqual(result, sorted_nums)
+        
+    def test_shell(self):
+        result = sorting.shell(nums)
+        self.assertIsNone(result)
+        self.assertEqual(result, sorted_nums)
+        
+    def test_quicksort(self):
+        result = sorting.quicksort(nums)
+        self.assertIsNone(result)
+        self.assertEqual(result, sorted_nums)
+        
+""" 
+if __name__ == "__main__":
+    test = test_sorting()
+    test.test_bubble()
+    test.test_selection()
+    test.test_insertion()
+"""
